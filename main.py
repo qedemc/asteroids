30c052b6397ca3fd0646ef6551e8aebc2dbaa2fd
qedemc <qed123emc@gmail.com>
2026-04-20 19:16:10 +0300

    add player in center

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -10,17 +11,27 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    clk = pygame.time.Clock()
+    dt = 0
+    
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            pass
+        
         screen.fill("black")
         
+        player.draw(screen)
+        
         pygame.display.flip()
+
+        dt = clk.tick(60) / 1000
 
 
 if __name__ == "__main__":
